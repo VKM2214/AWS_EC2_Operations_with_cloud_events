b6d0eb1b1a50d8f729c43a5e841305134e1738c1
VKM2214 <vigneshmurugankumar@gmail.com>
2021-07-26 21:11:09 +0530

    all1

---
+++ conf/credentials.py
@@ -0,0 +1,12 @@
+"""
+This config file would have the credentials 
+"""
+
+#aws
+aws_access_key_id= 'Enter your access key id'	
+aws_secret_access_key='Enter your secret access key'
+region='enter your region'
+
+#lambda function name
+lambda_function_name = 'Enter your start ec2 lambda function'
+lambda_function_name2 = 'Enter your stop ec2 lambda function'
